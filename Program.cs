8a27994f43e74e85cefe891bfe24d30bed606bd5
artem <art_nazarov@mail.ru>
2025-10-29 23:55:51 +0300

    Added logging

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddHttpLogging(o => { });  
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +17,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseHttpLogging();
 
 app.UseAuthorization();
 
